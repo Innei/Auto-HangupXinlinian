2041d10a4f808ea1db8d749165dbe663fd4de395
yiny0liuyin <yi00oi9hhg@qq.com>
2019-04-30 20:04:42 +0800

    v1

--- main.py
+++ main.py
@@ -3,53 +3,100 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
 import time
+from random import randint
+from time import sleep
+from selenium.webdriver.common.action_chains import ActionChains
 
 
 class hangUp():
-    browser = webdriver.Chrome()
-    browser.get('http://192.168.9.12/npels/')
-    wait = WebDriverWait(browser, 10)
 
     def __init__(self, username, passwd):
+
         self.username = username
         self.password = passwd
+        self.chrome_options = Options()
+        # 设置chrome浏览器无界面模式
+        self.chrome_options.add_argument('--headless')
+        self.browser = webdriver.Chrome(options=self.chrome_options)
+        self.browser.get('http://192.168.9.12/npels/')
+        self.wait = WebDriverWait(self.browser, 10)
+        self.n = 1
         self.time = 0
+        self.login()
+        self.intopage()
+        self.action = ActionChains(self.browser)
 
     def login(self):
-        hangUp.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#tbName'))).send_keys(self.username)
-        hangUp.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#tbPwd'))).send_keys(self.password)
-        hangUp.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#btnLogin'))).click()
+
+        self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#tbName'))).send_keys(self.username)
+        self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#tbPwd'))).send_keys(self.password)
+        self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#btnLogin'))).click()
 
     def intopage(self):
+
         try:
-            hangUp.browser.switch_to.frame('mainFrame')
-            start = hangUp.wait.until(EC.element_to_be_clickable(
+            self.browser.switch_to.frame('mainFrame')
+
+            try:
+                if self.browser.find_element_by_css_selector(
+                        'li.progress_' + str(
+                            self.n) + '>span').value_of_css_property('width') == '222px':
+                    self.n += 1
+            except:
+                self.browser.quit()
+
+            start = self.wait.until(EC.element_to_be_clickable(
                 (By.CSS_SELECTOR,
-                 '#aspnetForm > div.content > div.main_right > div:nth-child(2) > div > div.class_container > div > ul:nth-child(1) > a')))
+                 '#aspnetForm > div.content > div.main_right > div:nth-child(2) > div > div.class_container > div > ul:nth-child(' + str(
+                     self.n) + ') > a')))
+
             start.click()
             sleep(1)
-            hangUp.wait.until(EC.presence_of_element_located((By.LINK_TEXT,
-                                                              '继续学习'))).click()
+
+            self.wait.until(EC.presence_of_element_located((By.LINK_TEXT,
+                                                            '继续学习'))).click()
             sleep(1)
-            self.time = int(time.time())
+            self.testCompletion()
         except TimeoutError:
             return self.intopage()
 
+    def testCompletion(self):
+        print('开始学习了哦~')
+        self.time = int(time.time())
+        self.closeMessage()
+        self.aNext()
 
     def aNext(self):
-        if int(time.time()) - self.time > 2400:
-            nextP = hangUp.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#aNext')))
-            nextP.click()
+        self.browser.switch_to.frame('mainFrame')
+        try:
+            self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#aNext'))).click()
+
+        except:
+            print('没了哦~')
+            self.browser.quit()
+        print('切换成功了~')
+        self.testCompletion()
 
     def closeMessage(self):
-        try:
-            self.wait.until(EC.presence_of_element_located(
-                (By.CSS_SELECTOR, '#frameless > div.tcontent > div > input[type="button"]'))).click()
-        except TimeoutError:
-            return self.closeMessage()
+        self.browser.switch_to.default_content()  # 切换回主布局
+        while True:
+
+            print('当前时间', int(time.time()) - self.time)
+            try:
+                mess = self.wait.until(EC.element_to_be_clickable(
+                    (By.CSS_SELECTOR, '#frameless > div.tcontent > div > input[type="button"]'))).click()
+                if mess != None:
+                    self.action.move_to_element_with_offset(mess, 2, 1).click().perform()  # 点击按钮偏移量, 防检测
+            except:
+                pass
+            else:
+                print('检测到弹窗')
+            if int(time.time()) - self.time > 1080:
+                print('时间到了, 切换下一课')
+                break
+
 
 if __name__ == '__main__':
-    my = hangUp()
-    my.login()
-    my.intopage()
+    hangUp('username', 'passwd')
